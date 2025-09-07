41f1729011629478263f07151e7d8259204fa0ce
vanduong2704 <vanduong270499@gmail.com>
2025-09-07 21:09:14 +0700

    add router follow MVC

---
+++ src/route/web.js
@@ -0,0 +1,13 @@
+import express from  "express";
+import homeController from '../controller/homeController';
+let router = express.Router();
+
+const initWebRoute = (app) => {
+    router.get('/', homeController.getHomepage);
+    router.get('/about', (req, res) => {
+        res.send(`I'm Eric!`)
+    })
+    return app.use('/',router);
+}
+export default initWebRoute;
+// module.export = initWebRoute;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import configviewEngine from './config/viewEngine';
+import initWebRoute from './route/web';
 require('dotenv').config();
 
 const app = express();
 const port = process.env.port || 8081
 
+//setup view engine
 configviewEngine(app);
 
-app.get('/', (req, res) => {
-    res.render('test/index.ejs');
-})
-app.get('/about', (req, res) => {
-    res.send(`I'm Eric!`)
-})
+// init web route
+initWebRoute(app);
+
+
+
 
 
 app.listen(port, () => {
